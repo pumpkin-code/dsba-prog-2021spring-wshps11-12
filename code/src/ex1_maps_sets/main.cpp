c4891efbbcf0ca37a4a6003cf7c5807e86c8e8d4
Sergey <bjolyk@gmail.com>
2021-02-18 14:32:48 +0300

    Progress after the class of 18.02.2021.

--- code/src/ex1_maps_sets/main.cpp
+++ code/src/ex1_maps_sets/main.cpp
@@ -133,6 +133,102 @@ void printMap(const Int2SizetMap& coll)
 }
 
 
+// Prints out the keys only of the map in the forward order.
+void printMapKeys(const Int2SizetMap& coll)
+{
+    std::cout << "{";
+    for (Int2SizetMap::const_iterator it = std::begin(coll);        // std::begin(coll) === col.begin()
+         it != std::end(coll);                                      // std::end(coll) === col.end()
+         ++it)
+    {
+        //int& curKey = (*it).first;          // we can't provide a way to chanke a key
+                                            // we can't use reference, because it gives a semantics allowing to change of what cannot be changed.
+        const int& curKey = (*it).first;    // actually, for int it is better to copy vy value, however we wanted to show how one can consider an object w/o a copy by a (given new) name
+        std::cout << curKey << ", ";
+
+//        std::cout << (*it).first << ", ";   // an alternative w/o bindind another name for the object
+//        std::cout << it->first << ", ";   // yet another alternative w/o bindind another name for the object
+    }
+    std::cout << "}";
+}
+
+// Prints out the content of the map in the forward order by pairs of elements
+// skip[ing some element and printing each N-th element
+void printMapEachNthElement(const Int2SizetMap& coll, size_t n = 1)
+{
+    if (n == 0)
+        n = 1;                      // not the best way, however we may use this logic
+
+    size_t count2skip = n - 1;           // number of elements to skip
+    //Int2SizetMap::const_iterator it = std::begin(coll);
+    //while ( it != std::end(coll) )     // care with ++it inside the body of the loop
+    for (Int2SizetMap::const_iterator it = std::begin(coll);
+         it != std::end(coll);
+         ++it)
+    {
+        while (count2skip                // any integer is implicitly casted to the logical value: 0 is false, other wise it's true
+               && it != std::end(coll))
+        {
+            --count2skip;
+            ++it;
+        }
+
+        if(it == std::end(coll))
+            break;
+
+        std::cout << "{ " << it->first
+                  << ", " << it->second
+                  << " }\t";
+        //++it;                         // we must use it as the modifying section in the alternative version of the for loop while using the while loop
+
+        count2skip = n - 1;              // reset the counter for the next skip(s)
+    }
+}
+
+
+/// Looks for a key \a key,
+/// \returns true if the key if found and the corresponding value through \a val
+/// variable; otherwise returns false and the value of \a val is undefined.
+
+bool findKeyInMap(const Int2SizetMap& coll,         // a map where we gonna look for a key
+                  int key,                          // the key we a looking for
+                  size_t& val)                      // a ref. variable through which we return an associated value
+{
+    Int2SizetMap::const_iterator it = coll.find(key);
+    if (it == coll.end())
+        return false;
+
+    // if reach this point, the iterator is a valid existing element and we can dereference it
+    //(*it).second
+    val = it->second;                               // we copy a value to the variable outside of this function bound by the refereтce
+    return true;
+}
+// TODO: HW: Modify the method above ^^^^^ (create another (overloaded?) variant)
+// that returns a pair of <bool, int> with corresponding values.
+
+
+// Modifies the given map adding some given number for all values of map.
+Int2SizetMap& shiftMapsValues(Int2SizetMap& coll, size_t shiftVal)
+{
+    // TODO: HW: modify the method using the range-based for loop instead of the iterators
+
+    for (Int2SizetMap::iterator it = std::begin(coll);
+         it != std::end(coll);
+         ++it)
+    {
+        // again we want to give a name for another unnamed element of the map by using a reference
+        // now it's a NON-const reference, because we gonna use it change the object
+        size_t& val = it->second;       // *(it).second
+        val += shiftVal;                // (*(it).second) += shiftVal;    ====  (it->second) += shiftVal;
+    }
+
+    return coll;
+}
+
+
+// TODO: HW: Removes all negative keys from the map. Consider proper iterator re-initialization on erasing.
+
+
 
 
 int main()
@@ -150,6 +246,26 @@ int main()
     Int2SizetMap map1 = makeMapFromVector(vec);
     printMap(map1);
     std::cout << "\n";
+
+    std::cout << "Keys only: ";
+    printMapKeys(map1);
+    std::cout << "\n";
+
+    std::cout << "Printing each 2-nd element: ";
+    printMapEachNthElement(map1, 2);
+    std::cout << "\n";
+
+    //int val;                // can't bind an int with size_t& object
+    size_t val;
+    if (findKeyInMap(map1, 0, val))
+        std::cout << "Found a value for key = 0, val = " << val;
+
+    if (findKeyInMap(map1, 3, val))
+        std::cout << "Found a value for key = 3, val = " << val;
+
+
+    std::cout << "\n\nLet's add 10 to each value of the map: ";
+    printMap(shiftMapsValues(map1, 10));
 
 
     return 0;
